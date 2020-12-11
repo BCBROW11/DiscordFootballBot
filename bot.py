aea2fac1fbc76a2ffe5affc20df5a9da47144046
bcbrow11 <bcbrow11@asu.edu>
2020-12-10 18:34:58 -0800

    Fix scoring

--- bot.py
+++ bot.py
@@ -8,20 +8,22 @@ import http.client
 from bs4 import BeautifulSoup
 
 # Credentials
-TOKEN = '<BOTTOKEN>'
+TOKEN = '<TOKEN>'
 
 # Create bot
 client = commands.Bot(command_prefix='!')
 
 # Startup Information
 class game:
-    def __init__(self, home, homeScore, away, awayScore, status, time):
+    def __init__(self, day, home, homeScore, away, awayScore, status, timeInQuarter, gameTime):
+        self.day = day
         self.home = home
         self.homeScore = homeScore
         self.away = away
         self.awayScore = awayScore
         self.status = status
-        self.time = time
+        self.timeInQuarter = timeInQuarter
+        self.gameTime = gameTime
 
 @client.event
 async def on_ready():
@@ -44,6 +46,8 @@ async def standings(ctx):
     await ctx.send(
         len(items)
     )
+
+@client.command()
 async def scores(ctx):
 
     request = requests.get("http://static.nfl.com/liveupdate/scorestrip/scorestrip.json")
@@ -55,7 +59,6 @@ async def scores(ctx):
 
     scores = []
 
-    scoreEmbed = discord.Embed(color=0x00ff00)
     for i in y['ss']:
         if(i[2] == 'final overtime'):
             i[2] = 'Final'
@@ -66,32 +69,73 @@ async def scores(ctx):
             setting = "AM"
             if hours > 12:
                 setting = "PM"
-                hours -= 15
+                hours -= 12
             else:
-                hours -=3
-        scores.append(game(i[4], i[5], i[6], i[7], i[2], i[3])) #might be i[1] for time left
+                hours -= 3
+            i[1] = "{:02d}:{:02d}{}".format(hours, minutes, setting)
+        scores.append(game(i[0], i[4], i[5], i[6], i[7], i[2],i[3], i[1])) #might be i[1] for time left
 
-    scoreEmbed.color = discord.Color.green()
     gameCount = len(scores)
     gmStr = "```\n"
     x = 0
     for i in range(gameCount):
         if gameCount - x >= 3:
-            gmStr = gmStr + '{:5} {:10} {:5} {:10} {:5} {:10}'.format(scores[x].away, scores[x].awayScore, scores[x+1].away, scores[x+1].awayScore, scores[x+2].away, scores[x+2].awayScore)
-            gmStr = gmStr + "\n{:5} {:10} {:5} {:10} {:5} {:10}".format(scores[x].home, scores[x].homeScore, scores[x+1].home, scores[x+1].homeScore, scores[x+2].home, scores[x+2].homeScore)
-            gmStr = gmStr + "\n{:5} {:10} {:5} {:10} {:5} {:10}".format(scores[x].status, scores[x].time, scores[x+1].status, scores[x+1].time, scores[x+2].status, scores[x+2].time)
-            gmStr = gmStr + "\n\n"
-            x += 3
+            if scores[x].status == "Pregame" and scores[x+1].status == "Pregame" and scores[x+2].status == "Pregame":
+                gmStr = gmStr + '{:5}{:8}{:5}{:8}{:5}{:8}'.format(scores[x].away, scores[x].awayScore, scores[x+1].away, scores[x+1].awayScore, scores[x+2].away, scores[x+2].awayScore)
+                gmStr = gmStr + "\n{:5}{:8}{:5}{:8}{:5}{:8}".format(scores[x].home, scores[x].homeScore, scores[x+1].home, scores[x+1].homeScore, scores[x+2].home, scores[x+2].homeScore)
+                gmStr = gmStr + "\n{:3} {:9}{:3} {:9}{:3} {:9}".format(scores[x].day, scores[x].gameTime, scores[x+1].day, scores[x+1].gameTime, scores[x+2].day, scores[x+2].gameTime)
+                gmStr = gmStr + "\n\n"
+                x += 3
+            elif scores[x].status != "Pregame" and scores[x+1].status == "Pregame" and scores[x+2].status == "Pregame":
+                gmStr = gmStr + '{:5}{:8}{:5}{:8}{:5}{:8}'.format(scores[x].away, scores[x].awayScore, scores[x+1].away, scores[x+1].awayScore, scores[x+2].away, scores[x+2].awayScore)
+                gmStr = gmStr + "\n{:5}{:8}{:5}{:8}{:5}{:8}".format(scores[x].home, scores[x].homeScore, scores[x+1].home, scores[x+1].homeScore, scores[x+2].home, scores[x+2].homeScore)
+                gmStr = gmStr + "\nQ{:4}{:8}{:3} {:9}{:3} {:9}".format(scores[x].status, scores[x].timeInQuarter, scores[x+1].day, scores[x+1].gameTime, scores[x+2].day, scores[x+2].gameTime)
+                gmStr = gmStr + "\n\n"
+                x += 3
+            elif scores[x].status != "Pregame" and scores[x+1].status != "Pregame" and scores[x+2].status == "Pregame":
+                gmStr = gmStr + '{:5}{:8}{:5}{:8}{:5}{:8}'.format(scores[x].away, scores[x].awayScore, scores[x+1].away, scores[x+1].awayScore, scores[x+2].away, scores[x+2].awayScore)
+                gmStr = gmStr + "\n{:5}{:8}{:5}{:8}{:5}{:8}".format(scores[x].home, scores[x].homeScore, scores[x+1].home, scores[x+1].homeScore, scores[x+2].home, scores[x+2].homeScore)
+                gmStr = gmStr + "\nQ{:4}{:8}Q{:4}{:8}{:3} {:9}".format(scores[x].status, scores[x].timeInQuarter, scores[x+1].status, scores[x+1].timeInQuarter, scores[x+2].day, scores[x+2].gameTime)
+                gmStr = gmStr + "\n\n"
+                x += 3
+            else:
+                gmStr = gmStr + '{:5}{:8}{:5}{:8}{:5}{:8}'.format(scores[x].away, scores[x].awayScore, scores[x+1].away, scores[x+1].awayScore, scores[x+2].away, scores[x+2].awayScore)
+                gmStr = gmStr + "\n{:5}{:8}{:5}{:8}{:5}{:8}".format(scores[x].home, scores[x].homeScore, scores[x+1].home, scores[x+1].homeScore, scores[x+2].home, scores[x+2].homeScore)
+                gmStr = gmStr + "\nQ{:4}{:8}Q{:4}{:8}Q{:4}{:8}".format(scores[x].status, scores[x].timeInQuarter, scores[x+1].status, scores[x+1].timeInQuarter, scores[x+2].status, scores[x+2].timeInQuarter)
+                gmStr = gmStr + "\n\n"
+                x += 3
         elif gameCount - x == 2:
-            gmStr = gmStr + '{:5} {:10} {:5} {:10}'.format(scores[x].away, scores[x].awayScore, scores[x+1].away, scores[x+1].awayScore)
-            gmStr = gmStr + "\n{:5} {:10} {:5} {:10}".format(scores[x].home, scores[x].homeScore, scores[x+1].home, scores[x+1].homeScore)
-            gmStr = gmStr + "\n{:5} {:10} {:5} {:10}".format(scores[x].status, scores[x].time, scores[x+1].status, scores[x+1].time)
-            break
+            if scores[x].status == "Pregame" and scores[x+1].status == "Pregame":
+                gmStr = gmStr + '{:5}{:8}{:5}{:8}'.format(scores[x].away, scores[x].awayScore, scores[x+1].away, scores[x+1].awayScore)
+                gmStr = gmStr + "\n{:5}{:8}{:5}{:8}".format(scores[x].home, scores[x].homeScore, scores[x+1].home, scores[x+1].homeScore)
+                gmStr = gmStr + "\n{:3} {:9}{:3} {:9}".format(scores[x].day, scores[x].gameTime, scores[x+1].day, scores[x+1].gameTime)
+                gmStr = gmStr + "\n\n"
+                x += 2
+            elif scores[x].status != "Pregame" and scores[x+1].status == "Pregame":
+                gmStr = gmStr + '{:5}{:8}{:5}{:8}'.format(scores[x].away, scores[x].awayScore, scores[x+1].away, scores[x+1].awayScore)
+                gmStr = gmStr + "\n{:5}{:8}{:5}{:8}".format(scores[x].home, scores[x].homeScore, scores[x+1].home, scores[x+1].homeScore)
+                gmStr = gmStr + "\nQ{:4}{:8}{:3} {:9}".format(scores[x].status, scores[x].timeInQuarter, scores[x+1].day, scores[x+1].gameTime)
+                gmStr = gmStr + "\n\n"
+                x += 2
+            else:
+                gmStr = gmStr + '{:5}{:8}{:5}{:8}'.format(scores[x].away, scores[x].awayScore, scores[x+1].away, scores[x+1].awayScore)
+                gmStr = gmStr + "\n{:5}{:8}{:5}{:8}".format(scores[x].home, scores[x].homeScore, scores[x+1].home, scores[x+1].homeScore)
+                gmStr = gmStr + "\nQ{:4}{:8}Q{:4}{:8}".format(scores[x].status, scores[x].timeInQuarter, scores[x+1].status, scores[x+1].timeInQuarter)
+                gmStr = gmStr + "\n\n"
+                x += 2
         elif gameCount - x == 1:
-            gmStr = gmStr + '{:5} {:10}'.format(scores[x].away, scores[x].awayScore)
-            gmStr = gmStr + "\n{:5} {:10}".format(scores[x].home, scores[x].homeScore)
-            gmStr = gmStr + "\n{:5} {:10}".format(scores[x].status, scores[x].time)
-            break
+            if scores[x].status == "Pregame":
+                gmStr = gmStr + '{:5}{:8}'.format(scores[x].away, scores[x].awayScore)
+                gmStr = gmStr + "\n{:5}{:8}".format(scores[x].home, scores[x].homeScore)
+                gmStr = gmStr + "\n{:3} {:9}".format(scores[x].day, scores[x].gameTime)
+                gmStr = gmStr + "\n\n"
+                x += 1
+            else:
+                gmStr = gmStr + '{:5}{:8}'.format(scores[x].away, scores[x].awayScore)
+                gmStr = gmStr + "\n{:5}{:8}".format(scores[x].home, scores[x].homeScore)
+                gmStr = gmStr + "\nQ{:4}{:8}".format(scores[x].status, scores[x].timeInQuarter)
+                gmStr = gmStr + "\n\n"
+                x += 2
     gmStr = gmStr + "\n```"
 
 
